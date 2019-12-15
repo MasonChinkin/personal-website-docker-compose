d6d3f148e736c320c77b115e6ebc329e34243285
Mason Chinkin <mchinkin@gopro.com>
2019-12-15 14:46:29 -0800

    Configure for https

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(localhost)
+domains=(masonchinkin.com www.masonchinkin.com)
 rsa_key_size=4096
 data_path="./certbot"
 email="chinkinma@gmail.com" # Adding a valid address is strongly recommended
